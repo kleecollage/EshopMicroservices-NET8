b94e8feb9d10076e8131fe7a9d8f184682e0dcf2
kleeCollage <klee.collage@gmail.com>
2026-02-02 17:56:32 -0600

    refactor: Indent code

--- src/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
+++ src/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -8,14 +8,14 @@ public class GetProductsEndpoint: ICarterModule
     public void AddRoutes(IEndpointRouteBuilder app)
     {
         app.MapGet("/products", async (ISender sender) =>
-        {
-            var result = await sender.Send(new GetProductsQuery());
-            var response = result.Adapt<GetProductsResponse>();
-            return Results.Ok(response);
-        })
-        .WithName("GetProducts")
-        .Produces<GetProductsResponse>(StatusCodes.Status400BadRequest)
-        .WithSummary("Get Products")
-        .WithDescription("Get products");
+            {
+                var result = await sender.Send(new GetProductsQuery());
+                var response = result.Adapt<GetProductsResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("GetProducts")
+            .Produces<GetProductsResponse>(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Products")
+            .WithDescription("Get products");
     }
 }

0ae09289ab2794be49ede50f0bb909d90b696bfa
kleeCollage <klee.collage@gmail.com>
2026-02-04 13:35:32 -0600

    feat: Implement validation pipeline as interceptor for createProductHandler with fluentValidation

--- src/Catalog.API/GlobalUsing.cs
+++ src/Catalog.API/GlobalUsing.cs
@@ -1,7 +1,8 @@
 global using Carter;
 global using Mapster;
 global using MediatR;
-global using BuildingBlocks.CQRS;
 global using Marten;
+global using FluentValidation;
+global using BuildingBlocks.CQRS;
 global using Catalog.API.Models;
 global using Catalog.API.Exceptions;
--- src/Catalog.API/Program.cs
+++ src/Catalog.API/Program.cs
@@ -1,15 +1,25 @@
+using BuildingBlocks.Behaviors;
+
 var builder = WebApplication.CreateBuilder(args);
+var assembly = typeof(Program).Assembly;
 // SERVICES
-builder.Services.AddCarter();
 builder.Services.AddMediatR(config =>
 {
-    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
 });
+
+builder.Services.AddValidatorsFromAssembly(assembly);
+
+builder.Services.AddCarter();
+
 builder.Services.AddMarten(opts =>
 {
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);
     // opts.AutoCreateSchemaObjects()
 }).UseLightweightSessions();
+
+
 
 var app = builder.Build();
 // HTTP request pipeline

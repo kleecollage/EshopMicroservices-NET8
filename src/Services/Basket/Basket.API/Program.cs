76d9eef5c495215492b91a969ff13a51a84a9df0
kleeCollage <klee.collage@gmail.com>
2026-02-06 14:13:04 -0600

    refactor: Rename basket to basket.API

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,6 +1,13 @@
+// ==============================   Add services to the container   ============================== //
 var builder = WebApplication.CreateBuilder(args);
+
+
+
+
+
+// ==============================   Configure the HTTP request pipeline   ============================== //
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
 
 app.Run();

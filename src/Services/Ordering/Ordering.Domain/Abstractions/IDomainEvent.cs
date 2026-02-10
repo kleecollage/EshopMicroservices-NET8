5a7721ae17dce992a70072383959e518c5df173f
kleeCollage <klee.collage@gmail.com>
2026-02-10 16:48:05 -0600

    feat: Implement domain events on create and update order

--- src/Services/Ordering/Ordering.Domain/Abstractions/IDomainEvent.cs
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IDomainEvent.cs
@@ -5,6 +5,6 @@ namespace Ordering.Domain.Abstractions;
 public interface IDomainEvent : INotification
 {
     Guid EventId => Guid.NewGuid();
-    public DateTime OccurredOn { get; set; }
+    public DateTime OccurredOn => DateTime.Now;
     public string EventType => GetType().AssemblyQualifiedName;
 }

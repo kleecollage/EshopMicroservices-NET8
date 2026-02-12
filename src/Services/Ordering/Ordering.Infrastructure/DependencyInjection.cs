965cca5837a60a40954735f6695d084b5d3c816d
kleeCollage <klee.collage@gmail.com>
2026-02-11 22:59:10 -0600

    feat: Implement interceptors to retrieves create and update Domain events

--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -8,9 +9,15 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
         var connectionString = configuration.GetConnectionString("Database");
-        ////  Add services to the container
-        services.AddDbContext<ApplicationDbContext>(options =>
-             options.UseSqlServer(connectionString));
+        //  Add services to the container
+        services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptors>();
+        services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+        
+        services.AddDbContext<ApplicationDbContext>((sp, options) =>
+        {
+            options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+            options.UseSqlServer(connectionString);
+        });
         
         // services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
         
--- src/Services/Ordering/Ordering.Infrastructure/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Infrastructure/GlobalUsing.cs
@@ -3,3 +3,5 @@ global using Microsoft.EntityFrameworkCore.Metadata.Builders;
 global using Ordering.Domain.Models;
 global using Ordering.Domain.ValueObjects;
 global using Ordering.Infrastructure.Data;
+global using Ordering.Domain.Abstractions;
+global using Ordering.Infrastructure.Data.Interceptors;

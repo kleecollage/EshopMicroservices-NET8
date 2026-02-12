500110bb8e9bffa2096f2ad61d31cdc578afb146
kleeCollage <klee.collage@gmail.com>
2026-02-12 13:21:28 -0600

    feat: Implement CreateOrderHandler

--- src/Services/Ordering/Ordering.Application/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -3,3 +3,6 @@ global using FluentValidation;
 global using Microsoft.EntityFrameworkCore;
 global using BuildingBlocks.CQRS;
 global using Ordering.Application.Dtos;
+global using Ordering.Application.Data;
+global using Ordering.Domain.Models;
+global using Ordering.Domain.ValueObjects;
--- src/Services/Ordering/Ordering.Domain/Models/Order.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -4,7 +4,6 @@ public class Order : Aggregate<OrderId>
 {
     private readonly List<OrderItem> _orderItems = [];
     public IReadOnlyList<OrderItem> OrderItems => _orderItems.AsReadOnly();
-    
     public CustomerId CustomerId { get; private set; } = default!;
     public OrderName OrderName { get; private set; } = default!;
     public Address ShippingAddress { get; private set; } = default!;
